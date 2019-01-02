e7d0a321a80e21bfb8d2bcc8e16e5a70431765b3
madeshv5543 <madeshv5543@gmail.com>
2019-01-02 15:56:31 +0530

    initial commit

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 80));
-app.use(express.static(__dirname + '/build'));
-app.set('views', __dirname + '/build');
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
