881be57f0489ad9b071927ea260e2533b3586bac
madeshv5543 <madeshv5543@gmail.com>
2019-01-02 15:58:12 +0530

    initial commit

---
+++ public/precache-manifest.1590b3c9b5eef5705b6714c924bf202b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "655c33a3e46da3e28c64",
+    "url": "/static/js/main.655c33a3.chunk.js"
+  },
+  {
+    "revision": "fa625a4b3107cd1f4c6c",
+    "url": "/static/js/1.fa625a4b.chunk.js"
+  },
+  {
+    "revision": "655c33a3e46da3e28c64",
+    "url": "/static/css/main.87b4baf1.chunk.css"
+  },
+  {
+    "revision": "a4cdc4a475d124c3535a42c086be1ae7",
+    "url": "/index.html"
+  }
+];
