bd7ae4691d17a7bdc2d6c58e0fa78c6b05550da7
madeshv5543 <madeshv5543@gmail.com>
2019-01-02 15:37:29 +0530

    hide history

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ app.use(staticFileMiddleware)
 // app.use(history())
 app.use(staticFileMiddleware)
 // ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
-app.set('view engine', 'html');
 app.get('/', function (req, res) {
   res.render(path.join(__dirname ,'/build'))
 })
 
-app.listen(80, function () {
+app.listen(3000, function () {
   console.log( 'Express serving on 80!' )
 })
