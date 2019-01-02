d165a183e5c28b07e0ef13b4b77367286899bb24
madeshv5543 <madeshv5543@gmail.com>
2019-01-02 15:54:01 +0530

    view engine

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const path = require('path')
-// const history = require('connect-history-api-fallback')
-// ^ middleware to redirect all URLs to index.html
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const app = express()
+var app = express();
+
+app.set('port', (process.env.PORT || 80));
 app.use(express.static(__dirname + '/build'));
+app.set('views', __dirname + '/build');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 
 
-// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname ,'/build/index.html'))
-})
+app.get('*', function(req, res){
+    res.render('index.html');
+});
 
-app.listen(80, function () {
-  console.log( 'Express serving on 80!' )
-})
+app.listen(app.get('port'), function() {
+});
