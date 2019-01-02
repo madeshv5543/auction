628370b4a5f8c3542a5ada3155ab88bfdfb659ad
madeshv5543 <madeshv5543@gmail.com>
2019-01-02 15:31:46 +0530

    hide history

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const path = require('path')
-const history = require('connect-history-api-fallback')
+// const history = require('connect-history-api-fallback')
 // ^ middleware to redirect all URLs to index.html
 
 const app = express()
 const staticFileMiddleware = express.static(path.join(__dirname, '/build'))
 
 app.use(staticFileMiddleware)
-app.use(history())
+// app.use(history())
 app.use(staticFileMiddleware)
 // ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
 
